7f59aab6dfbce2b8a7e3d8ee2a1193a0ca648c83
Gabriella Garavito <48166887+Gaby1003@users.noreply.github.com>
2023-01-09 12:43:03 -0500

    Add exception format

--- Reto dos.py
+++ Reto dos.py
@@ -1,3 +1,5 @@
+import sys
+
 def orderDatas(squares):
   for j in range(len(squares) - 1):
       for i in range(len(squares) - j - 1):
@@ -11,11 +13,16 @@ s = 5
 print('Ingrese el arreglo en el siguiente formato: {"array": [-6, -5, 0, 5, 6]}')
 datas = input()
 if datas:
-  data = eval(datas)
-  data = [int(x) for x in data.get("array")]
+  try:
+    data = eval(datas)
+    data = [int(x) for x in data.get("array")]
+  except:
+    print("el formato ingresado no es correcto")
+    sys.exit()
+    
   squares = [x*x for x in data if x*x in range(0, int(f"{s}{s}"))]
   squares = orderDatas(squares)
   print(squares)
-  
+ 
 else:
   print("El arreglo no cuenta con datos")
